124cb0e2bac1df1d907554c43b4b82ea11ad03be
Hank Tom <hanktom@gmail.com>
2020-06-16 10:55:19 +0800

    Car, two function

---
+++ src/main/java/com/tom/Car.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Car {
+    String id;
+    long enter;
+    long leave;
+
+    public Car(String id) {
+        this.id = id;
+        enter = System.currentTimeMillis();
+    }
+
+}
